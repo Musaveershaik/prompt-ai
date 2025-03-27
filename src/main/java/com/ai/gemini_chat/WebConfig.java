bd1a249ae94a2fe13195ed4b317a801029f1ca71
musa <abdulmusawwirshaik@gmail.com>
2025-03-28 02:39:50 +0530

    edited webConfig.java

--- src/main/java/com/ai/gemini_chat/WebConfig.java
+++ src/main/java/com/ai/gemini_chat/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://ai-prompt-optimizer.netlify.app/")
+                .allowedOrigins("https://ai-prompt-optimizer.netlify.app", "http://localhost:3000")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

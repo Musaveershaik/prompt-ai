52474ce4c28b0dfca0a8105ccab2a3901f28b399
musa <abdulmusawwirshaik@gmail.com>
2025-03-28 03:06:29 +0530

    edited webConfig.java

--- src/main/java/com/ai/gemini_chat/AIController.java
+++ src/main/java/com/ai/gemini_chat/AIController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "*")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/qna")
